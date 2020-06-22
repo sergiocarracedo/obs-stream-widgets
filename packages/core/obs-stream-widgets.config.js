c116e542b522586c3563e13bbf001aaddaf3a8ae
Sergio Carracedo <info@sergiocarracedo.es>
2020-06-22 16:51:45 +0200

    Rename widgets packages names

--- packages/core/obs-stream-widgets.config.js
+++ packages/core/obs-stream-widgets.config.js
@@ -1,8 +1,8 @@
 export default {
   widgets: {
-    'widget-brand': require('widget-brand'),
-    'widget-chat': require('widget-chat'),
-    'widget-sponsors': require('widget-sponsors'),
-    'widget-countdown': require('widget-countdown')
+    'widget-brand': require('@obs-stream-widgets/widget-brand'),
+    'widget-chat': require('@obs-stream-widgets/widget-chat'),
+    'widget-sponsors': require('@obs-stream-widgets/widget-sponsors'),
+    'widget-countdown': require('@obs-stream-widgets/widget-countdown')
   }
 }
