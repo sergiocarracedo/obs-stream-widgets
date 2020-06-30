e0b309619394ddc9b24adef0d66059d8b71910f2
Sergio Carracedo <info@sergiocarracedo.es>
2020-06-30 18:12:23 +0200

    Fix Theme style error

--- src/components/ThemeStyles.vue
+++ src/components/ThemeStyles.vue
@@ -5,28 +5,23 @@ import { Map } from '@/types'
 
 export default Vue.extend({
   name: 'theme-style',
-  methods: {
-    themeStyles () {
-      let css: string[] = []
-
-      const theme = (store.state as unknown as Map<any>).theme
-      css.push(`.primary-color { color: ${theme.primary};}`)
-      css.push(`.primary-stroke { stroke: ${theme.primary};}`)
-      css.push(`.primary-border { border-color: ${theme.primary};}`)
-      css.push(`.primary-bg { background-color: ${theme.primary};}`)
-
-      css.push(`.primary-light-color { color: ${theme.primaryLight};}`)
-      css.push(`.primary-light-stroke { stroke: ${theme.primaryLight};}`)
-      css.push(`.primary-light-border { border-color: ${theme.primaryLight};}`)
-      css.push(`.primary-light-bg { background-color: ${theme.primaryLight};}`)
-
-      css.push('.text-dark { color: #000 }')
-
-      return css.join('\n')
-    }
-  },
   render: function (createElement) {
-    return createElement('style', this.themeStyles())
+    const css: string[] = []
+
+    const theme = (store.state as unknown as Map<any>).theme
+    css.push(`.primary-color { color: ${theme.primary};}`)
+    css.push(`.primary-stroke { stroke: ${theme.primary};}`)
+    css.push(`.primary-border { border-color: ${theme.primary};}`)
+    css.push(`.primary-bg { background-color: ${theme.primary};}`)
+
+    css.push(`.primary-light-color { color: ${theme.primaryLight};}`)
+    css.push(`.primary-light-stroke { stroke: ${theme.primaryLight};}`)
+    css.push(`.primary-light-border { border-color: ${theme.primaryLight};}`)
+    css.push(`.primary-light-bg { background-color: ${theme.primaryLight};}`)
+
+    css.push('.text-dark { color: #000 }')
+
+    return createElement('style', css.join('\n'))
   }
 })
 </script>
